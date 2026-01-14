65620efe52084f38a24cbcd58d2f820653a37bc5
Shreyansh <shreyanshsharma1010@gmail.com>
2026-01-13 22:46:57 -0800

    LC-107

---
+++ binary tree/bTreeLevelOrder2.cpp
@@ -0,0 +1,47 @@
+// LC-107: Binary Tree Level Order Traversal II
+
+// Only one modification on top of LC-102 
+// Just reverse the ans array before returning it
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+vector<vector<int>> levelOrderBottom(TreeNode* root) {
+        vector<vector<int>> ans; 
+        if (!root) return ans;   
+
+        queue<TreeNode*> q;
+
+        q.push(root);
+        
+        while (!q.empty()) {
+            int levelSize = q.size();
+            vector<int> levelNodes;
+            
+            for (int i = 0; i<levelSize; i++) {
+                TreeNode* curr = q.front();
+                q.pop(); 
+                
+                levelNodes.push_back(curr->val);
+                
+                if(curr->left != nullptr) q.push(curr->left);
+                if(curr->right) q.push(curr->right);
+            }
+            
+            ans.push_back(levelNodes);
+        }
+
+        reverse(ans.begin(), ans.end());
+
+        return ans;
+    }

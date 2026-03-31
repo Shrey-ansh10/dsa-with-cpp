e1d77fa242d09079605be5ad549af30df3d777b2
Shreyansh <shreyanshsharma1010@gmail.com>
2026-03-31 15:58:12 -0700

    some small changes

--- binary search tree/validateBST.cpp
+++ binary search tree/validateBST.cpp
@@ -24,7 +24,7 @@ public:
         if(root->val <= min) return false;
         if(root->val >= max) return false;
         
-        // if we dont stick in base case - recursive calls on left and right sub tree
+        // if we don't stuck in base case - recursive calls on left and right sub tree
         return (validateBinTree(root->left, min, root->val) && validateBinTree(root->right, root->val, max));
     }
 
@@ -32,7 +32,7 @@ public:
         return validateBinTree(root, LLONG_MIN, LLONG_MAX); // call the helper function and return it's value
         
         // The constrains of this problem statement states the value on each node will be INT_MIN <= node->val <= INT_MAX
-        // So that means we can use Long long in our solution, but before that there is someones responsibility to check if the value at each node is in that range. 
+        // So that means we can use Long long in our solution, but before that there is someone's responsibility to check if the value at each node is in that range. 
 
         // If we change the condition a little bit to INT_MIN < node->val < INT_MAX , using INT_MIN and INT_MAX will work perfectly fine. 
         // It's just that the as soon as the <= comes in the constrain, that's when INT_MIN and INT_MAX no longer will work

eb81869591270a5eb14a60915bbf2671d38bda37
Shreyansh <shreyanshsharma1010@gmail.com>
2026-05-22 16:05:52 -0700

    LC-116 Sol-2(more space efficint)

--- binary tree/nextRightPointer.cpp
+++ binary tree/nextRightPointer.cpp
@@ -1,5 +1,12 @@
 // LC-116. Populating Next Right Pointers in Each Node
 
+#include<iostream>
+using namespace std;
+
+#include<queue>
+
+
+
 class Node {
 public:
     int val;
@@ -52,5 +59,33 @@ public:
         }
         return root;
         
+    }
+};
+
+class solution2{ // this one has a better space complexity
+public:
+    Node* connect(Node* root) { // TC: O(n), SC: O(1)
+        if(root == nullptr) return root;
+
+        Node* leftmost = root; //initialize the leftmost with root, this will be to track the level down the tree 
+        leftmost->next = nullptr; // as we know before starting that next of root is always nullptr
+
+        while(leftmost->left){
+            Node* curr = leftmost; // taking a current pointer whcih will be used to travers throught a level of the tree
+
+            while(curr){//till current is not null
+
+                //connect current's left's next to right
+                curr->left->next = curr->right;
+
+                //current's right to left of next's
+                if(curr->next) curr->right->next = curr->next->left;
+                else curr->right = nullptr; // if it's the rightmost node at a level - point it's next to null
+
+                curr = curr->next; // update the current to the next node on same level
+            }
+            leftmost = leftmost->left; // moving to the next level downward the tree
+        }
+        return root;
     }
 };
